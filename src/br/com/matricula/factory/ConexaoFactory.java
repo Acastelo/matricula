acd611da888bafb10682b548a47d66f809d9f194
Luiz Alexandre <luiz.alexandre@dellead.com>
2017-12-05 09:29:51 -0300

    Correção da classe de factory para inclusão do jpa

--- src/br/com/matricula/factory/ConexaoFactory.java
+++ src/br/com/matricula/factory/ConexaoFactory.java
@@ -7,26 +7,21 @@ import java.sql.SQLException;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import javax.sql.DataSource;
 
 
 public class ConexaoFactory {
 	
+	private static EntityManagerFactory factory;
 	
-	public static Connection getConexao() throws NamingException, SQLException {
-		 
-		Context	ctx = new InitialContext();
-		DataSource	ds = (DataSource) ctx.lookup("java:/comp/env/jdbc/matricula_bd");
-	    return ds.getConnection();		
-		
-		
-	}	
-	public static void main(String[] args) {
-		try {
-			System.out.println(getConexao());
-		} catch (NamingException | SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	public static EntityManager getInstancia(){
+		if (factory == null ){
+			factory = Persistence.createEntityManagerFactory("matriculapadroes");
 		}
+		
+		return factory.createEntityManager();
 	}
 }
--- src/br/com/matricula/model/UsuarioModel.java
+++ src/br/com/matricula/model/UsuarioModel.java
@@ -2,13 +2,16 @@ package br.com.matricula.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-public class UsuarioModel implements Serializable {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-
+@Entity
+public class UsuarioModel {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 
 	private String login;
